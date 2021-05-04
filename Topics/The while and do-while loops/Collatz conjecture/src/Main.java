2e67d3aa08bb5fa521f6ea091643507e039eedef
Nursultan <83649608+NursulTan-os@users.noreply.github.com>
2021-05-04 18:46:09 +0300

    commit

--- Topics/The while and do-while loops/Collatz conjecture/src/Main.java
+++ Topics/The while and do-while loops/Collatz conjecture/src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-        // put your code here
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), collatz = n;
+        while (collatz != 1) {
+            System.out.print(collatz + " ");
+            collatz = collatz % 2 == 0 ? collatz / 2 : collatz * 3 +1;
+        }
+        System.out.print(collatz + " ");
     }
 }
--- Topics/The while and do-while loops/Profit/src/Main.java
+++ Topics/The while and do-while loops/Profit/src/Main.java
@@ -2,6 +2,15 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Scanner sc = new Scanner(System.in);
+        double m = sc.nextInt();
+        double p = sc.nextInt();
+        double k = sc.nextInt();
+        int c = 0;
+        while ( m < k ) {
+            m = m * ((p/100)+1);
+            c++;
+        }
+        System.out.println(c);
     }
 }
